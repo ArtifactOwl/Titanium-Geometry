73be5828a826fdb3ad97199d022d3a3df8f945f5
Artifact Owl <jaekle@gmail.com>
2025-08-06 22:18:11 -0400

    imgsize

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,13 @@ export default function Home() {
 
       <main style={{ marginTop: "3rem", textAlign: "center" }}>
         <h1>Titanium Pendant Necklaces</h1>
-        <p>Hand-crafted. Uniquely yours.</p>
+        <p>Unique titanium pendants, keychain fobs, and tools with unique precision engraving, cutting, and coloring.</p>
 
         <h2 style={{ marginTop: "2rem" }}>Sample Designs</h2>
         <div style={{ display: "flex", justifyContent: "center", gap: "1rem", marginTop: "1rem" }}>
-          <img src="/orbitals.jpg" alt="Sample 1" style={{ width: "1644px", height: "1344px" }} />
-          <img src="/selection.jpg" alt="Sample 2" style={{ width: "1966px", height: "1394px" }} />
-          <img src="/dmtmolecule.jpg" alt="Sample 3" style={{ width: "2400px", height: "1550px" }} />
+          <img src="/orbitals.jpg" alt="Sample 1" style={{ width: "250px", height: "250px" }} />
+          <img src="/selection.jpg" alt="Sample 2" style={{ width: "250px", height: "250px" }} />
+          <img src="/dmtmolecule.jpg" alt="Sample 3" style={{ width: "250px", height: "250px" }} />
         </div>
       </main>
     </div>
