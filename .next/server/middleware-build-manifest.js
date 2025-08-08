759fd5a72c36cc9040a1998f90477dc4a14e4711
Artifact Owl <jaekle@gmail.com>
2025-08-08 13:44:37 -0400

    Update global layout in _app.js

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -9,6 +9,11 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -19,10 +24,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/products/dmtpr": [
+    "/contact": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/products/dmtpr.js"
+      "static/chunks/pages/contact.js"
     ]
   },
   "ampFirstPages": []
